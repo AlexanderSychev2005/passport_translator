3d38d0e8bf87cdeffa42fe3f89dfa3c01831b9b3
Alex <alexandersychev2005@gmail.com>
2025-03-04 22:52:27 +0200

    Updated code

--- passport_scanner.py
+++ passport_scanner.py
@@ -3,47 +3,59 @@ from datetime import datetime
 import easyocr
 import cv2
 from reportlab.pdfbase.ttfonts import TTFont
-from googletrans import Translator
 from reportlab.lib.pagesizes import A4
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfgen import canvas
 
-translator = Translator()
-
+# Register fonts
 pdfmetrics.registerFont(TTFont('DejaVuSans', 'DejaVuSans.ttf'))
 pdfmetrics.registerFont(TTFont('DejaVuSansBold', 'DejaVuSans-Bold.ttf'))
 
-countries_map = {
+# Define translations for countries
+COUNTRIES_TRANSLATION = {
     "UKR": "UKRAYNA", "TUR": "TÜRKİYE", "USA": "AMERİKA BİRLEŞİK DEVLETLERİ",
     "RUS": "RUSYA", "DEU": "ALMANYA", "FRA": "FRANSA"
 }
 
-month_translation = {
+# Define translations for months
+MONTH_TRANSLATION = {
     "Jan": "OCAK", "Feb": "ŞUBAT", "Mar": "MART", "Apr": "NİSAN",
     "May": "MAYIS", "Jun": "HAZİRAN", "Jul": "TEMMUZ", "Aug": "AĞUSTOS",
     "Sep": "EYLÜL", "Oct": "EKİM", "Nov": "KASIM", "Dec": "ARALIK"
 }
 
 
+def translate_date(date_str):
+    day, month, year = date_str.split()
+    month_turkish = MONTH_TRANSLATION.get(month, month)
+    return f"{day} {month_turkish} {year}"
+
+
 def save_to_file(filename, data):
+    """
+    Function to save passport data to a PDF file
+    :param filename: filename of the file
+    :param data: dictionary of passport data
+    :return: None
+    """
     c = canvas.Canvas(filename, pagesize=A4)
     c.setFont("DejaVuSansBold", 14)
-    c.drawString(270, 750, data["DOĞUM YERİ"])
+    c.drawString(270, 750, data["Full country"])
     c.setFont("DejaVuSansBold", 10)
     c.drawString(50, 700, "PASAPORT")
     c.drawString(170, 700, "P")
-    c.drawString(190, 550, f"{data["CİNSİYETİ"]}")
+    c.drawString(190, 550, f"{data["Gender"]}")
 
-    c.drawString(250, 670, f"{data["surname"]}")
-    c.drawString(250, 640, f"{data["name"]}")
-    c.drawString(250, 610, f"{data["UYRUĞU"]}")
-    c.drawString(250, 580, f"{data["DOĞUM TARİHİ"]}")
+    c.drawString(250, 670, f"{data["Surname"]}")
+    c.drawString(250, 640, f"{data["Name"]}")
+    c.drawString(250, 610, f"{data["Full country"]}")
+    c.drawString(250, 580, f"{data["Date of birth"]}")
+    c.drawString(370, 550, f"{data["Full country"]}")
+    c.drawString(250, 520, f"{data["Date of issue"]}")
+    c.drawString(250, 490, f"{data["Date of expiry"]}")
 
-    c.drawString(250, 520, f"{data["DÜZENLENME TARİHİ"]}")
-    c.drawString(250, 490, f"{data["GEÇERLİLİK TARİHİ"]}")
-
-    c.drawString(450, 700, f"{data["PASAPORT NO"]}")
-    c.drawString(490, 520, f"{data["DÜZENLEYEN MAKAM"]}")
+    c.drawString(450, 700, f"{data["Passport number"]}")
+    c.drawString(490, 520, f"{data["Authority"]}")
     c.setFont("DejaVuSans", 10)
 
     c.drawString(50, 600, "")
@@ -57,12 +69,11 @@ def save_to_file(filename, data):
     c.drawString(130, 520, "DÜZENLENME TARİHİ:")
     c.drawString(130, 490, "GEÇERLİLİK TARİHİ:")
 
-    c.drawString(250, 700, f"ÜLKE KODU: {data["ÜLKE KODU"]}")
+    c.drawString(250, 700, f"ÜLKE KODU: {data["Country"]}")
     c.drawString(250, 550, f"DOĞUM YERİ:")
 
     c.drawString(370, 700, f"PASAPORT NO.:")
-    c.drawString(370, 580, f"KAYIT NO.:{data["KAYIT NO"]}")
-    c.drawString(370, 550, f"{data["DOĞUM YERİ"]}")
+    c.drawString(370, 580, f"KAYIT NO.:{data["Record number"]}")
     c.drawString(370, 520, f"DÜZENLEYEN MAKAM:")
 
     c.setFont("DejaVuSans", 7)
@@ -72,118 +83,86 @@ def save_to_file(filename, data):
     c.save()
 
 
-image = cv2.imread("oleg_test.jpg")
+def main():
+    image = cv2.imread("oleg_test.jpg")
 
-detail = cv2.detailEnhance(image, sigma_s=20, sigma_r=0.15)
-gray = cv2.cvtColor(detail, cv2.COLOR_BGR2GRAY)
+    reader = easyocr.Reader(['en', 'uk'], gpu=True)
+    result = reader.readtext(image,
+                             detail=0,
+                             contrast_ths=0.5,
+                             adjust_contrast=0.7,
+                             text_threshold=0.6,
+                             low_text=0.4,
+                             canvas_size=2000,
+                             decoder='wordbeamsearch')
 
-# cv2.imshow("gray", gray)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+    full_text = " ".join(result)
+    entities = {
+        "Орган що видав/ Authority": re.search(r"(\d{4})", full_text),
+        "MRZ": re.search(r"(P\s*<[^\n]*\d{2})", full_text),
+    }
+    filtered_entities = {k: v.group(1) if v else None for k, v in entities.items()}
+    filtered_entities['MRZ'] = filtered_entities['MRZ'].replace(" ", '')
 
-reader = easyocr.Reader(['en', 'uk'], gpu=True)
-result = reader.readtext(image,
-                         detail=0,
-                         contrast_ths=0.5,
-                         adjust_contrast=0.7,
-                         text_threshold=0.6,
-                         low_text=0.4,
-                         canvas_size=2000,
-                         decoder='wordbeamsearch')
+    mrz = filtered_entities['MRZ'].upper()
 
-full_text = " ".join(result)
-entities = {
-    "Орган що видав/ Authority": re.search(r"(\d{4})", full_text),
-    "MRZ": re.search(r"(P\s*<[^\n]*\d{2})", full_text),
-}
-filtered_entities = {k: v.group(1) if v else None for k, v in entities.items()}
-filtered_entities['MRZ'] = filtered_entities['MRZ'].replace(" ", '')
+    authority = filtered_entities['Орган що видав/ Authority']
 
-mrz = filtered_entities['MRZ'].upper()
+    mrz = mrz.replace("О", "O").replace("М", "M")
+    mrz_pattern = re.compile(
+        r"P<(?P<country>[A-Z]{3})"
+        r"(?P<surname>[A-Z]+)<<(?P<name>[A-Z]+)<+"
+        r"(?P<passport_number>[A-Z0-9]{8})<+"
+        r"."
+        r"(?P<nationality>[A-Z]{3})"
+        r"(?P<birth_date>\d{6})"
+        r"."
+        r"(?P<gender>[MF])"
+        r"(?P<expiry_date>\d{6})"
+        r"."
+        r"(?P<personal_number>[A-Z0-9]{13})"
+    )
+    match = mrz_pattern.search(mrz)
 
-authority = filtered_entities['Орган що видав/ Authority']
+    if match:
+        country = match.group("country")
+        name = match.group("name")
+        surname = match.group("surname")
+        passport_number = match.group("passport_number")
+        nationality = match.group("nationality")
+        birth_date = match.group("birth_date")
+        gender = match.group("gender")
+        expiry_date = match.group("expiry_date")
+        personal_number = match.group("personal_number")
 
-mrz = mrz.replace("О", "O").replace("М", "M")
-print(mrz)
-mrz_pattern = re.compile(
-    r"P<(?P<country>[A-Z]{3})"
-    r"(?P<surname>[A-Z]+)<<(?P<name>[A-Z]+)<+"
-    r"(?P<passport_number>[A-Z0-9]{8})<+"
-    r"."
-    r"(?P<nationality>[A-Z]{3})"
-    r"(?P<birth_date>\d{6})"
-    r"."
-    r"(?P<gender>[MF])"
-    r"(?P<expiry_date>\d{6})"
-    r"."
-    r"(?P<personal_number>[A-Z0-9]{13})"
-)
-match = mrz_pattern.search(mrz)
+    date_obj = datetime.strptime(birth_date, "%y%m%d")
+    formatted_date = date_obj.strftime("%d %b %Y")
 
-if match:
-    country = match.group("country")
-    name = match.group("name")
-    surname = match.group("surname")
-    passport_number = match.group("passport_number")
-    nationality = match.group("nationality")
-    birth_date = match.group("birth_date")
-    gender = match.group("gender")
-    expiry_date = match.group("expiry_date")
-    personal_number = match.group("personal_number")
+    date_obj_exp = datetime.strptime(expiry_date, "%y%m%d")
+    formatted_date_exp = date_obj_exp.strftime("%d %b %Y")
 
-date_obj = datetime.strptime(birth_date, "%y%m%d")
-formatted_date = date_obj.strftime("%d %b %Y")
+    issue_date_obj = date_obj_exp.replace(year=date_obj_exp.year - 10)
+    formatted_date_issue = issue_date_obj.strftime("%d %b %Y")
 
-date_obj_exp = datetime.strptime(expiry_date, "%y%m%d")
-formatted_date_exp = date_obj_exp.strftime("%d %b %Y")
+    country_full = COUNTRIES_TRANSLATION[country]
+    parsed_results = {
+        "Country": f"{country}",
+        "Surname": f"{surname}",
+        "Name": f"{name}",
+        "Passport number": f"{passport_number}",
+        "Nationality": f"{nationality}",
+        "Full country": f"{country_full}",
+        "Date of birth": f"{translate_date(formatted_date)}",
+        "Gender": "KADIN" if gender == 'F' else "ADAM",
+        "Authority": f"{authority}",
+        "Date of issue": f"{translate_date(formatted_date_issue)}",
+        "Date of expiry": f"{translate_date(formatted_date_exp)}",
+        "Record number": f"{personal_number[:8]}-{personal_number[8:]}",
+        "MRZ": mrz
+    }
 
-issue_date_obj = date_obj_exp.replace(year=date_obj_exp.year - 10)
-formatted_date_issue = issue_date_obj.strftime("%d %b %Y")
-
-
-def translate_date(date_str):
-    day, month, year = date_str.split()
-    month_turkish = month_translation.get(month, month)
-    return f"{day} {month_turkish} {year}"
-
-country_full = countries_map[country]
-parsed_results = {
-    "Country": f"{country}",
-    "Surname": f"{surname}",
-    "Name": f"{name}",
-    "Passport number": f"{passport_number}",
-    "Nationality": f"{nationality}",
-    "Place of birth": f"{country_full}",
-    "Date of birth": f"{translate_date(formatted_date)}",
-    "Gender": "KADIN" if gender == 'F' else "ADAM",
-    "Authority": f"{authority}",
-    "Date of issue": f"{translate_date(formatted_date_issue)}",
-    "Date of expiry": f"{translate_date(formatted_date_exp)}",
-    "Record number": f"{personal_number[:8]}-{personal_number[8:]}",
-    "mrz": mrz
-}
+    save_to_file("passport_data.pdf", parsed_results)
 
 
-parsed_data = {
-    "ÜLKE KODU": country,
-    "PASAPORT NO": passport_number,
-    "SOYADI": surname,
-    "ADI": name,
-    "UYRUĞU": country,
-    "DOĞUM TARİHİ": translate_date(formatted_date),
-    "CİNSİYETİ": "KADIN" if gender == 'F' else "ADAM",
-    "DOĞUM YERİ": country_full,
-    "DÜZENLENME TARİHİ": translate_date(formatted_date_issue),
-    "GEÇERLİLİK TARİHİ": translate_date(formatted_date_exp),
-    "KAYIT NO": f"{personal_number[:8]}-{personal_number[8:]}",
-    "DÜZENLEYEN MAKAM": authority,
-    "MRZ": mrz
-}
-print(len(mrz))
-#
-# for i in parsed_data:
-#     print(f"{i}: {parsed_data[i]}")
-# print("\n")
-
-save_to_file("passport_data.pdf", parsed_data)
-# print("!!!")
+if __name__ == "__main__":
+    main()
