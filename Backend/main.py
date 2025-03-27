24fed411d4439e5262c008e59b629a46df39945e
OlehLC <o.lobko2018@gmail.com>
2025-03-27 15:59:49 +0200

    Add scaning documents and some front

--- Backend/main.py
+++ Backend/main.py
@@ -35,10 +35,10 @@ def scandoc():
         if four_points is None:
             message = 'UNABLE TO LOCATE THE CORDANATES'
             points = [
-                {'x': 10, 'y': 10},
-                {'x': 320, 'y': 10},
-                {'x': 320, 'y': 420},
-                {'x': 10, 'y': 420}
+                {'x': 0, 'y': 10999999},
+                {'x': 0, 'y': 0},
+                {'x': 0, 'y': 0},
+                {'x': 0, 'y': 0}
             ]
             return render_template('scaner.html', points=points, fileupload=True,
                                    message=message)
@@ -49,6 +49,31 @@ def scandoc():
 
     return render_template("scaner.html")
 
+@app.route('/scandoc', methods=['GET', 'POST'])
+def scan():
+    if request.method == 'POST':
+        file = request.files['image_name']
+        upload_image_path = utils.save_upload_image(file)
+        print('Img save in =', upload_image_path)
+        # predict the coordianates
+        four_points, size = docscan.document_scanner(upload_image_path)
+        print(four_points, size)
+        if four_points is None:
+            message = 'UNABLE TO LOCATE THE CORDANATES'
+            points = [
+                {'x':0 , 'y': 0},
+                {'x': 420, 'y':0},
+                {'x': 0, 'y': 420},
+                {'x': 420, 'y': 420}
+            ]
+            return render_template('scanerdoc.html', points=points, fileupload=True,
+                                   message=message)
+        else:
+            points = utils.array_to_json_format(four_points)
+            message = 'Located the Cordinets of Document  '
+            return render_template('scanerdoc.html', points=points, fileupload=True, message=message)
+
+    return render_template("scanerdoc.html")
 
 @app.route('/transform', methods=['POST'])
 def transform():
@@ -76,11 +101,11 @@ def prediction():
 @app.route('/file_translation')
 def file_translation():
     wrap_image_filepath = settings.join_path(settings.MEDIA_DIR, 'magic_color.jpg')
-    translated_test, html_with_entities_name = document_results.getData(wrap_image_filepath)
+    translated_text, html_with_entities_name = document_results.getData(wrap_image_filepath)
     html_filepath = settings.join_path(settings.MEDIA_DIR, html_with_entities_name)
     with open(html_filepath, 'r', encoding='utf-8') as f:
         html_with_entities = f.read()
-    return render_template("file_translation.html", translated_test=translated_test, html_with_entities=html_with_entities)
+    return render_template("file_translation.html", translated_text=translated_text, html_with_entities=html_with_entities)
 
 if __name__ == "__main__":
     app.run(debug=True)
