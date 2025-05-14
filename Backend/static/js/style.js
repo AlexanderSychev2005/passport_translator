cb71d70fb954543da2398f6f65e3d31c63079a7c
Alex <alexandersychev2005@gmail.com>
2025-05-14 11:32:32 +0300

    Deleted everything

--- Backend/static/js/style.js
+++
@@ -1,9 +0,0 @@
-const checkbox = document.querySelector('.navigation__checkbox');
-
-checkbox.addEventListener('change', () => {
-  if (checkbox.checked) {
-    document.body.classList.add('no-scroll');
-  } else {
-    document.body.classList.remove('no-scroll');
-  }
-});
